85880966ac6d9fd2a608bf38ceb994eb2d5d78ec
Yermolenko-Alevel <yermolenko-alevel@users.noreply.github.com>
2018-12-20 01:57:36 +0200

    Done with Navigation System and finished (I hope so) with bootstrap

---
+++ Store.MVC/Controllers/NavigationController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StoreLogic.Abstract;
+
+namespace Store.MVC.Controllers
+{
+    public class NavigationController : Controller
+    {
+        private ICakeRepository cakeRepository;
+
+        public NavigationController (ICakeRepository cakeRepo)
+        {
+            cakeRepository = cakeRepo;
+        }
+
+        public PartialViewResult Menu()
+        {
+            IEnumerable<string> categories = cakeRepository.Cakes
+                .Select(cake => cake.Category)
+                .Distinct()
+                .OrderBy(x => x);
+            return PartialView(categories);
+        }
+
+        // GET: Navigation
+        //public string Index()
+        //{
+        //    return "TESTING";
+        //}
+    }
+}
