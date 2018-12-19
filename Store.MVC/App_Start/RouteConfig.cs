c517405b4a2f97d6b0cddf6301bd99a830993a85
Yermolenko-Alevel <yermolenko-alevel@users.noreply.github.com>
2018-12-19 17:34:17 +0200

    Created nice URL for List of all cakes. RouteConfig.cs

--- Store.MVC/App_Start/RouteConfig.cs
+++ Store.MVC/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace Store.MVC
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: null,
+                url: "Cakes/Page{page}",
+                defaults: new { controller = "Cake", action = "List"}
+                );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Cake", action = "List", id = UrlParameter.Optional }
