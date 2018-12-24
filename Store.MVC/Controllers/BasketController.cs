45d56ba7d8643aa1ea011279b38059aaed290de9
Yermolenko-Alevel <yermolenko-alevel@users.noreply.github.com>
2018-12-24 03:00:40 +0200

    Finished with Basket. Finally.

--- Store.MVC/Controllers/BasketController.cs
+++ Store.MVC/Controllers/BasketController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Store.MVC.Models;
 using StoreLogic.Abstract;
 using StoreLogic.Entities;
 
@@ -16,12 +17,60 @@ namespace Store.MVC.Controllers
             repository = repo;
         }
 
-        
+        public ViewResult Index(string urlReturn)
+        {
+            return View(new BasketIndexView
+            {
+                Basket = GetBasket(),
+                UrlReturn = urlReturn
+            });
+
+        }
+
+        public RedirectToRouteResult AddToBasket(int id, string urlReturn)
+        {
+            Cake cake = repository.Cakes
+                .FirstOrDefault(c => c.Id == id);
+
+            if (cake != null)
+            {
+                GetBasket().Add(cake, 1);
+            }
+
+            return RedirectToAction("Index", new { urlReturn });
+        }
+
+        public RedirectToRouteResult RemoveFromBasket(int id, string urlReturn)
+        {
+            Cake cake = repository.Cakes
+                .FirstOrDefault(c => c.Id == id);
+
+            if (cake != null)
+            {
+                GetBasket().RemoveLine(cake);
+            }
+
+            return RedirectToAction("Index", new { urlReturn });
+        }
+
+
+        public Basket GetBasket()
+        {
+            Basket basket = (Basket)Session["Basket"];
+            if (basket == null)
+            {
+                basket = new Basket();
+                Session["Basket"] = basket;
+            }
+            return basket;
+
+        }
+
 
         // GET: Basket
-        public ActionResult Index()
-        {
-            return View();
-        }
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
     }
 }
--- Store.MVC/Models/CakeListView.cs
+++ Store.MVC/Models/CakeListView.cs
@@ -1,4 +1,5 @@
-﻿using StoreLogic.Entities;
+﻿using StoreLogic.Abstract;
+using StoreLogic.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,4 +14,35 @@ namespace Store.MVC.Models
         public string CurrentCategory { get; set; }
 
     }
-}
+
+    //public class CakeListLogic
+    //{
+    //    private ICakeRepository cakeRepository { get; set; }
+    //    public int pageSize = 4;
+
+
+    //    public CakeListView Logic(CakeListView model, int pageSize, string category)
+    //    {
+    //        CakeListView model = new CakeListView
+    //        {
+    //            Cakes = cakeRepository.Cakes
+    //                    .Where(c => category == null || c.Category == category)
+    //                    .OrderBy(cake => cake.Id)
+    //                    .Skip((page - 1) * pageSize)
+    //                    .Take(pageSize),
+    //            PagesInfo = new PageInfo
+    //            {
+    //                CurrentPage = page,
+    //                ItemsPerPage = pageSize,
+    //                TotalItems = category == null ? //???
+    //             cakeRepository.Cakes.Count() :
+    //             cakeRepository.Cakes.Where(cake => cake.Category == category)
+    //                                 .Count()
+    //            },
+    //            CurrentCategory = category
+    //        };
+    //    };
+
+
+
+    }
